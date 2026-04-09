44e593ca30059e0768f9baeae19e5682a0c34a5b
Yi Zhang <y_zh426@outlook.com>
2026-04-09 12:31:29 +0100

    Basic operations

--- plugin/filter/src/lib.rs
+++ plugin/filter/src/lib.rs
@@ -1,4 +1,31 @@
+
+
 #[unsafe(no_mangle)]
 fn get_secret_number() -> u32 {
     1116
+}
+
+// Allocate a fixed size of memory and return the raw pointer
+#[unsafe(no_mangle)]
+fn allocate_memory(size: usize) -> *mut u8 {
+    let vec = Vec::with_capacity(size);
+
+    let (ptr, _len, _cap) = vec.into_raw_parts();
+
+    let ptr = ptr as *mut u8;
+    ptr
+}
+
+// Main filter function
+#[unsafe(no_mangle)]
+fn process_request(ptr: *const u8, len: usize) -> u8 {
+    let result_str = unsafe {std::slice::from_raw_parts(ptr, len)};
+    let val = serde_json::from_str(ptr)
+    if result_str == "suspicious" {1} else {0}
+}
+
+// Free memory
+#[unsafe(no_mangle)]
+fn free_memory(ptr : *mut u8, len: usize) {
+    unsafe{ Vec::from_raw_parts(ptr, len, len);}
 }
