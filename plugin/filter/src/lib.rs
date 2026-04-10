7a55cfa67a77a45d625862db64ab1942042d97c6
Yi Zhang <y_zh426@outlook.com>
2026-04-10 11:50:41 +0100

    Let Rust process header in JSON

--- plugin/filter/src/lib.rs
+++ plugin/filter/src/lib.rs
@@ -1,5 +1,3 @@
-
-
 #[unsafe(no_mangle)]
 fn get_secret_number() -> u32 {
     1116
@@ -19,13 +17,30 @@ fn allocate_memory(size: usize) -> *mut u8 {
 // Main filter function
 #[unsafe(no_mangle)]
 fn process_request(ptr: *const u8, len: usize) -> u8 {
-    let result_str = unsafe {std::slice::from_raw_parts(ptr, len)};
-    let val = serde_json::from_str(ptr)
-    if result_str == "suspicious" {1} else {0}
+    // Read slice
+    let raw_data = unsafe {std::slice::from_raw_parts(ptr, len)};
+    // Construct JSON
+    let val: Result<serde_json::Value, _> = serde_json::from_slice(raw_data);
+    // Check header
+    match val {
+        Ok(header) => {
+            let is_suspicious = header.get("Block")
+                .and_then(|values| values.get(0))
+                .and_then(|first_val| first_val.as_str())
+                .map(|s| s == "suspicious")
+                .unwrap_or(false);
+
+            if is_suspicious { 1 } else { 0 }
+        },
+        Err(_) => {
+            0
+        }
+    }
 }
 
 // Free memory
 #[unsafe(no_mangle)]
-fn free_memory(ptr : *mut u8, len: usize) {
-    unsafe{ Vec::from_raw_parts(ptr, len, len);}
+fn free_memory(ptr : *mut u8, len: usize, cap: usize) {
+    // The Vec goes out of scope and auto deallocated
+    unsafe{ let _ = Vec::from_raw_parts(ptr, len, cap);}
 }
