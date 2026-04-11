4b5838d90c957939e5baca3005e270da8c01554f
Yi Zhang <y_zh426@outlook.com>
2026-04-11 15:52:29 +0100

    Support concurrency

--- proxy/cmd/main/main.go
+++ proxy/cmd/main/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -10,65 +9,14 @@ import (
 	"os"
 
 	"github.com/tetratelabs/wazero"
-	"github.com/tetratelabs/wazero/api"
 )
 
 const (
 	backend_url    = "http://localhost:9090"
 	listen_on_port = ":8080"
 	wasm_path      = "../plugin/filter/target/wasm32-unknown-unknown/release/filter.wasm"
-	block_header   = "Block"
+	num_channel    = 32
 )
-
-func middleware(ctx context.Context, mod api.Module,
-	allocate api.Function, process_request api.Function, free_memory api.Function, proxy *httputil.ReverseProxy) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		header_json, err := json.Marshal(r.Header)
-		if err != nil {
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			log.Printf("JSON marshal error: %v", err)
-			return
-		}
-
-		// Allocate memory
-		ptr, err := allocate.Call(ctx, uint64(len(header_json)))
-		if err != nil {
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			log.Printf("Wasm allocation error: %v", err)
-			return
-		}
-
-		// Free memory afterwards
-		defer func() {
-			_, err := free_memory.Call(ctx, ptr[0], uint64(len(header_json)), uint64(len(header_json)))
-			if err != nil {
-				log.Printf("Failed to free Wasm memory: %v", err)
-			}
-		}()
-
-		// Write header
-		if ok := mod.Memory().Write(uint32(ptr[0]), header_json); !ok {
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			return
-		}
-
-		// Check header
-		res, err := process_request.Call(ctx, ptr[0], uint64(len(header_json)))
-		if err != nil {
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			log.Printf("Wasm execution error: %v", err)
-			return
-		}
-
-		// Block if enabled
-		if res[0] == 1 {
-			http.Error(w, "Forbidden", http.StatusForbidden)
-			return
-		}
-
-		proxy.ServeHTTP(w, r)
-	}
-}
 
 func main() {
 	// Parse upstream server url
@@ -88,28 +36,46 @@ func main() {
 	r := wazero.NewRuntime(ctx)
 	defer r.Close(ctx)
 
-	// Instantiate wasm
-	mod, err := r.Instantiate(ctx, file)
+	// Compile wasm module
+	compiledMod, err := r.CompileModule(ctx, file)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Load allocation
-	allocate := mod.ExportedFunction("allocate_memory")
-	if allocate == nil {
-		log.Fatal("Function allocate not found")
-	}
+	// Create a buffered channel
+	channel := make(chan Instance, num_channel)
 
-	// Load check header
-	process_request := mod.ExportedFunction("process_request")
-	if process_request == nil {
-		log.Fatal("Function process_request not found")
-	}
+	// Instantiate modules and put them into channel
+	for i := 0; i < num_channel; i++ {
+		// Instantiate a module
+		mod, err := r.InstantiateModule(ctx, compiledMod, wazero.NewModuleConfig().WithName("")) // Generate a random name for the instance
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	// Load free memory
-	free_memory := mod.ExportedFunction("free_memory")
-	if free_memory == nil {
-		log.Fatal("Function free_memory not found")
+		// Load allocation
+		allocate := mod.ExportedFunction("allocate_memory")
+		if allocate == nil {
+			log.Fatal("Function allocate not found")
+		}
+
+		// Load check header
+		process_request := mod.ExportedFunction("process_request")
+		if process_request == nil {
+			log.Fatal("Function process_request not found")
+		}
+
+		// Load free memory
+		free_memory := mod.ExportedFunction("free_memory")
+		if free_memory == nil {
+			log.Fatal("Function free_memory not found")
+		}
+
+		// Create an instance struct
+		instance := Instance{mod, allocate, process_request, free_memory}
+
+		// Put into channel
+		channel <- instance
 	}
 
 	// Instantiate a reverse proxy engine
@@ -122,7 +88,7 @@ func main() {
 	}
 
 	// Create a middleware
-	wrapped_handler := middleware(ctx, mod, allocate, process_request, free_memory, proxy)
+	wrapped_handler := middleware(ctx, channel, proxy)
 
 	// Listen on port
 	http.ListenAndServe(listen_on_port, wrapped_handler)
